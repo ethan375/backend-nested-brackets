424e368357fe5b00788d105ff3d2cc3ec1574ea5
ethan375 <ethanebel1@gmail.com>
2019-11-07 13:07:22 -0600

    almost there, just need to include char position

--- nested.py
+++ nested.py
@@ -7,49 +7,51 @@ __author__ = "Ethan375"
 
 import sys
 
-def main(args):
-    input_file = open(args[1],'r')
-    read_file = input_file.read()
 
-    split_input = read_file.split('\n')
-    refined_list = []
-    for val in split_input:
-        if val != '':
-            refined_list.append(val)
-    print(refined_list)
+def check_nested(string):
+
+    stack = []
+
+    open_chars = ['(*', '(', '[', '{', '<']
+    closed_chars = ['*)', ')', ']', '}', '>']
+
+    properly_nested = False
+
+    while string:
+        o_chars = 0
+        c_chars = 0
     
 
-    open_chars = 0
-    closed_chars = 0 
-    for string in refined_list:
-        for s in string:
-            if s == '(*':
-                open_chars += 1
-            elif s == '(':
-                open_chars += 1 
-            elif s == '[':
-                open_chars += 1 
-            elif s == '{':
-                open_chars += 1 
-            elif s == '*)':
-                closed_chars += 1 
-            elif s == ')':
-                closed_chars += 1 
-            elif s == '(':
-                closed_chars += 1 
-            elif s == '(':
-                closed_chars += 1 
-            elif s == '(':
-                closed_chars += 1
-            if closed_chars > open_chars:
-                return 'theres been a problem'
-    print(open_chars)
-    print(closed_chars)
-    if open_chars != closed_chars:
-        return 'something went wrong'
-    else:
-        return 'something went just fine'
-    input_file.close
+        split_string = list(string)
+        for enumerate(i), char in split_string:
+            if char == '(' and split_string[i + 1] == '*':
+                o_chars += 1
+            elif char == '*' and split_string[i + 1] == ')':
+                c_chars += 1
+            elif char in open_chars:
+                o_chars += 1
+            elif char in closed_chars:
+                c_chars += 1
+        if o_chars == c_chars:
+            return 'Yes'
+        else:
+            return 'No'
+                
+
+
+
+
+def main(args):
+    input_file = args[1]
+
+    with open(input_file, "r") as file:
+        file = file.read()   
+        split_input = file.split('\n')
+
+    with open("output.txt", "w") as out:
+        for line in split_input:
+            out.write(check_nested(line) + '\n')
+    
 
 if __name__ == '__main__':
     main(sys.argv)
